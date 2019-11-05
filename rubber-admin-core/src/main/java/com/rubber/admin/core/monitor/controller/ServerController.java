fdb7f67a681bb4b977c6698af2577d385e86845b
yugaofeng <Ygf@33520>
2019-11-05 20:53:17 +0800

    添加服务的监控信息

---
+++ rubber-admin-core/src/main/java/com/rubber/admin/core/monitor/controller/ServerController.java
@@ -0,0 +1,25 @@
+package com.rubber.admin.core.monitor.controller;
+
+import cn.hutool.coocaa.util.result.ResultMsg;
+import com.rubber.admin.core.monitor.Server;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author luffyu
+ * Created on 2019-11-05
+ * 系统服务器的监控信息
+ */
+@RestController
+@RequestMapping(value = "/monitor/server",name = "server")
+public class ServerController {
+
+
+    @GetMapping("/info")
+    public ResultMsg getInfo() throws Exception {
+        Server server = new Server();
+        server.copyTo();
+        return ResultMsg.success(server);
+    }
+}
