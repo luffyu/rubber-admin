f65b77dd2cbaaf71396479a628de8b2f1f5a70e0
yugaofeng <Ygf@33520>
2020-01-13 20:51:24 +0800

    添加CORS系统平台

---
+++ rubber-admin-security/src/main/java/com/rubber/admin/security/config/CORSFilter.java
@@ -0,0 +1,44 @@
+package com.rubber.admin.security.config;
+
+
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * <p>添加过滤时间</p>
+ *
+ * @author luffyu
+ * @date 2019-12-18 16:52
+ **/
+@SuppressWarnings("Duplicates")
+@Order(value = 1)
+@WebFilter
+public class CORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        response.setHeader("Access-Control-Allow-Origin",   "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
+
--- rubber-admin-test/src/main/java/com/rubber/admin/test/RubberAdminTestApplication.java
+++ rubber-admin-test/src/main/java/com/rubber/admin/test/RubberAdminTestApplication.java
@@ -2,8 +2,10 @@ package com.rubber.admin.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class RubberAdminTestApplication {
 
     public static void main(String[] args) {
