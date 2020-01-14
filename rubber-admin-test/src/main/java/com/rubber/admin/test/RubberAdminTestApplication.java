a6e663a18a269665d9afc9d7b3329f89fd71d1db
yugaofeng <Ygf@33520>
2020-01-14 16:36:36 +0800

    管理后台添加mybatis的自动配置信息

--- rubber-admin-test/src/main/java/com/rubber/admin/test/RubberAdminTestApplication.java
+++ rubber-admin-test/src/main/java/com/rubber/admin/test/RubberAdminTestApplication.java
@@ -2,7 +2,6 @@ package com.rubber.admin.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 public class RubberAdminTestApplication {
