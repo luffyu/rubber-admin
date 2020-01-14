76362fb55771f008842e29989a4b370a80d4a379
yugaofeng <Ygf@33520>
2020-01-14 16:36:13 +0800

    管理后台添加mybatis的自动配置信息

--- rubber-admin-test/src/main/java/com/rubber/admin/test/RubberAdminTestApplication.java
+++ rubber-admin-test/src/main/java/com/rubber/admin/test/RubberAdminTestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
 public class RubberAdminTestApplication {
 
     public static void main(String[] args) {
--- springboot-rubber-admin-start/src/main/java/com/rubber/admin/springboot/start/RubberAdminAutoConfiguration.java
+++ springboot-rubber-admin-start/src/main/java/com/rubber/admin/springboot/start/RubberAdminAutoConfiguration.java
@@ -1,9 +1,12 @@
 package com.rubber.admin.springboot.start;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.rubber.admin.core.plugins.cache.ICacheProvider;
 import com.rubber.admin.core.plugins.cache.LocalCacheProvider;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.rubber.admin.**")
 @MapperScan("com.rubber.admin.**.mapper.**")
+@ServletComponentScan
 public class RubberAdminAutoConfiguration {
 
 
@@ -27,5 +31,24 @@ public class RubberAdminAutoConfiguration {
         return new LocalCacheProvider();
     }
 
+
+    /**
+     * 开启MybatisPlus分页插件
+     */
+    @Bean
+    @ConditionalOnMissingBean(PaginationInterceptor.class)
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+
+    /**
+     * 开启MyBatisPlus乐观锁插件
+     */
+    @Bean
+    @ConditionalOnMissingBean(OptimisticLockerInterceptor.class)
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
 
 }
