b6182e7b37016291686b259bd1e8e78995415d08
yugaofeng <Ygf@33520>
2020-04-03 17:45:55 +0800

    添加自己的sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,21 +1,82 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : docker_local_3308
+ Source Server         : 本地电脑
  Source Server Type    : MySQL
- Source Server Version : 80018
- Source Host           : localhost:3308
+ Source Server Version : 50727
+ Source Host           : localhost:3306
  Source Schema         : rubber_admin
 
  Target Server Type    : MySQL
- Target Server Version : 80018
+ Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 07/11/2019 16:53:24
+ Date: 03/04/2020 17:44:56
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for auth_group_config
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_group_config`;
+CREATE TABLE `auth_group_config` (
+  `group_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '部门id',
+  `group_key` varchar(50) NOT NULL DEFAULT '' COMMENT '组关键字',
+  `group_name` varchar(50) NOT NULL DEFAULT '' COMMENT '组名称',
+  `group_type` varchar(50) NOT NULL DEFAULT '' COMMENT '组类型 ',
+  `group_member` varchar(500) DEFAULT '' COMMENT '组成员 多个组成员用英文逗号隔开',
+  `status` tinyint(3) DEFAULT '0' COMMENT '状态（0正常 -1停用）',
+  `update_by` int(11) DEFAULT NULL COMMENT '最后一次更新人id',
+  `update_time` datetime DEFAULT NULL COMMENT '最好一个更新时间',
+  `remark` varchar(500) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`group_id`),
+  KEY `idx_auth_group` (`group_key`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='权限族群配置表';
+
+-- ----------------------------
+-- Records of auth_group_config
+-- ----------------------------
+BEGIN;
+INSERT INTO `auth_group_config` VALUES (1, 'add', '增加', 'option', 'add,save,install,saving,copy', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (2, 'delete', '删除', 'option', 'del,delete,remove,rf', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (3, 'update', '修改', 'option', 'edit,update,modify,mod', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (4, 'select', '查询', 'option', 'list,query,get,find,page,info,download,export,select,read', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (5, 'menu', '菜单管理', 'apply', 'menu', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (6, 'permission', '权限管理', 'apply', 'permission', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (7, 'role', '角色管理', 'apply', 'role', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (8, 'user', '用户管理', 'apply', 'user', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (9, 'dept', '部门权限', 'apply', 'dept', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (10, 'monitor', '监控', 'apply', 'monitor', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (11, 'u', '用户基础权限', 'apply', 'u', 0, NULL, NULL, NULL);
+INSERT INTO `auth_group_config` VALUES (13, 'auth', '权限管理', 'apply', 'auth', 0, NULL, NULL, NULL);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for auth_group_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_group_menu`;
+CREATE TABLE `auth_group_menu` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '部门id',
+  `menu_id` varchar(50) NOT NULL DEFAULT '' COMMENT '组关键字',
+  `option_key` varchar(50) NOT NULL DEFAULT '' COMMENT '操作组的key',
+  `related_apply` varchar(255) NOT NULL DEFAULT '' COMMENT '相关应用关键字，多个关键字用英文,隔开',
+  PRIMARY KEY (`id`),
+  KEY `idx_meun` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COMMENT='权限族和菜单管理表';
+
+-- ----------------------------
+-- Records of auth_group_menu
+-- ----------------------------
+BEGIN;
+INSERT INTO `auth_group_menu` VALUES (22, '15', 'add', 'role,menu');
+INSERT INTO `auth_group_menu` VALUES (23, '15', 'tree', 'dept');
+INSERT INTO `auth_group_menu` VALUES (28, '16', 'add', 'auth');
+INSERT INTO `auth_group_menu` VALUES (29, '16', 'update', 'auth');
+INSERT INTO `auth_group_menu` VALUES (30, '16', 'select', 'auth');
+INSERT INTO `auth_group_menu` VALUES (31, '16', 'delete', 'auth');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for sys_dept
@@ -26,7 +87,7 @@ CREATE TABLE `sys_dept` (
   `parent_id` int(11) DEFAULT '0' COMMENT '父部门id 0表示根目录',
   `dept_name` varchar(30) DEFAULT '' COMMENT '部门名称',
   `seq` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '现实排序 最大支持 255',
-  `leader` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '负责人',
+  `leader` varchar(20) NOT NULL COMMENT '负责人',
   `phone` varchar(11) DEFAULT NULL COMMENT '联系电话',
   `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
   `status` tinyint(3) DEFAULT '0' COMMENT '部门状态（0正常 -1停用）',
@@ -37,13 +98,17 @@ CREATE TABLE `sys_dept` (
   `update_time` datetime DEFAULT NULL COMMENT '最好一个更新时间',
   `remark` varchar(500) DEFAULT '' COMMENT '备注',
   PRIMARY KEY (`dept_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='部门表';
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COMMENT='部门表';
 
 -- ----------------------------
 -- Records of sys_dept
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_dept` VALUES (1, 0, '云技术部门', 0, '路飞', '1999', 'luffyu@onepiece.com', 0, 0, 1, '2019-11-07 16:52:18', 1, '2019-11-07 16:52:20', '');
+INSERT INTO `sys_dept` VALUES (9, 0, '这是一个测试', 1, '余', '18617181447', NULL, 0, 0, 1, '2020-01-31 14:35:44', 1, '2020-01-31 15:32:23', '');
+INSERT INTO `sys_dept` VALUES (11, 0, '草帽海贼团', 3, '余', '18617181447', NULL, 0, 0, 1, '2020-01-31 15:29:14', 1, '2020-01-31 15:29:53', '');
+INSERT INTO `sys_dept` VALUES (12, 11, '一分队', 1, '余1', '18617181447', NULL, 0, 0, 1, '2020-01-31 15:30:37', 1, '2020-01-31 15:30:37', '');
+INSERT INTO `sys_dept` VALUES (13, 11, '二分队', 2, '余rap', '18617181447', NULL, 0, 0, 1, '2020-01-31 15:30:54', 1, '2020-01-31 15:30:54', '');
+INSERT INTO `sys_dept` VALUES (14, 9, '这是一个测试-1212', 2, '12', '18617181447', NULL, 0, 0, 1, '2020-01-31 15:31:07', 1, '2020-01-31 15:32:36', '');
 COMMIT;
 
 -- ----------------------------
@@ -67,17 +132,44 @@ CREATE TABLE `sys_menu` (
   `update_time` datetime DEFAULT NULL COMMENT '最好一个更新时间',
   `remark` varchar(500) DEFAULT '' COMMENT '备注',
   PRIMARY KEY (`menu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='菜单权限表';
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COMMENT='菜单权限表';
 
 -- ----------------------------
 -- Records of sys_menu
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_menu` VALUES (1, '系统管理', 0, 0, '#', 'M', NULL, 0, 0, '#', NULL, NULL, NULL, NULL, '');
-INSERT INTO `sys_menu` VALUES (2, '菜单管理', 1, 1, '#', 'C', NULL, 0, 0, '#', NULL, NULL, NULL, NULL, '');
-INSERT INTO `sys_menu` VALUES (3, '角色管理', 1, 2, '#', 'C', NULL, 0, 0, '#', NULL, NULL, NULL, NULL, '');
-INSERT INTO `sys_menu` VALUES (4, '用户管理', 1, 3, '#', 'C', NULL, 0, 0, '#', NULL, NULL, NULL, NULL, '');
-INSERT INTO `sys_menu` VALUES (6, '部门管理', 1, 0, '#', 'C', NULL, 0, 0, '#', 1, '2019-11-05 18:05:59', 1, '2019-11-05 18:12:45', '这是一个词而是');
+INSERT INTO `sys_menu` VALUES (1, '系统管理', 0, 0, '#', 'M', NULL, 0, 0, 'el-icon-lx-home', NULL, NULL, 1, '2020-01-16 11:26:45', '');
+INSERT INTO `sys_menu` VALUES (2, '菜单管理', 1, 7, '/sys/menu-view', 'C', NULL, 0, 0, '#', NULL, NULL, 1, '2020-03-14 11:22:40', '');
+INSERT INTO `sys_menu` VALUES (3, '角色管理', 1, 6, '/sys/role-view', 'C', NULL, 0, 0, 'tag', NULL, NULL, 1, '2020-01-31 18:00:20', '');
+INSERT INTO `sys_menu` VALUES (4, '用户管理', 1, 4, '/sys/user-view', 'C', NULL, 0, 0, '#', NULL, NULL, 1, '2020-01-31 18:00:25', '');
+INSERT INTO `sys_menu` VALUES (6, '部门管理', 1, 3, '/sys/dept-view', 'C', NULL, 0, 0, '#', 1, '2019-11-05 18:05:59', 1, '2020-02-01 13:27:08', '这是一个词而是');
+INSERT INTO `sys_menu` VALUES (13, '权限字典', 1, 2, '/sys/permission-dict-view', 'C', NULL, -1, 0, '#', 1, '2020-01-31 17:29:25', 1, '2020-03-25 15:44:06', '');
+INSERT INTO `sys_menu` VALUES (14, '业务列表', 17, 0, '/sys/authorize-view', 'C', NULL, 0, 0, '#', 1, '2020-03-13 15:04:31', 1, '2020-03-25 15:52:03', '');
+INSERT INTO `sys_menu` VALUES (15, '测试', 1, 10, '/test/tt', 'C', NULL, -1, 0, '#', 1, '2020-03-13 21:14:32', 1, '2020-03-25 15:43:44', '');
+INSERT INTO `sys_menu` VALUES (16, '权限配置', 17, 11, '/sys/authorize-config', 'C', NULL, 0, 0, '', 1, '2020-03-19 10:51:03', 1, '2020-03-25 15:55:45', '');
+INSERT INTO `sys_menu` VALUES (17, '权限管理', 1, 11, '#', 'M', NULL, 0, 0, '#', 1, '2020-03-25 15:48:57', 1, '2020-03-25 15:49:37', '');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for sys_menu_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu_permission`;
+CREATE TABLE `sys_menu_permission` (
+  `mp_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `menu_id` int(11) DEFAULT '0' COMMENT '菜单id',
+  `button_key` varchar(50) DEFAULT '' COMMENT '菜单里面的按钮Id',
+  `button_name` varchar(255) DEFAULT NULL COMMENT '按钮名称',
+  `basic_module` varchar(50) DEFAULT '' COMMENT '关联的权限一级目录',
+  `basic_unit` varchar(50) DEFAULT '' COMMENT '关联的权限二级目录',
+  PRIMARY KEY (`mp_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='菜单权限配置关联表';
+
+-- ----------------------------
+-- Records of sys_menu_permission
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_menu_permission` VALUES (2, 2, 'add', NULL, 'menu', 'add');
+INSERT INTO `sys_menu_permission` VALUES (3, 2, 'add', NULL, 'permission', 'add,select');
 COMMIT;
 
 -- ----------------------------
@@ -98,7 +190,7 @@ CREATE TABLE `sys_permission_dict` (
   `remark` varchar(500) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`),
   KEY `idx_pri_key` (`dict_key`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='权限字典名称';
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='权限字典名称';
 
 -- ----------------------------
 -- Records of sys_permission_dict
@@ -115,7 +207,9 @@ INSERT INTO `sys_permission_dict` VALUES (8, 'menu', '菜单管理', 'menu', 'ba
 INSERT INTO `sys_permission_dict` VALUES (9, 'permission', '权限管理', 'permission', 'basic_module', 0, NULL, NULL, NULL, NULL, NULL);
 INSERT INTO `sys_permission_dict` VALUES (10, 'role', '角色管理', 'role', 'basic_module', 0, NULL, NULL, NULL, NULL, NULL);
 INSERT INTO `sys_permission_dict` VALUES (11, 'user', '用户管理', 'user', 'basic_module', 0, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `sys_permission_dict` VALUES (12, 'dect', '部门管理', 'dect', 'basic_module', 0, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `sys_permission_dict` VALUES (12, 'dept', '部门权限', 'dept', 'basic_module', 0, NULL, NULL, 1, '2020-02-01 16:39:15', NULL);
+INSERT INTO `sys_permission_dict` VALUES (13, 'monitor', '监控', 'monitor', 'basic_module', 0, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `sys_permission_dict` VALUES (15, 'u', '用户基础权限', 'login,u,error,test', 'basic_module', 0, NULL, NULL, 1, '2020-02-01 16:43:34', NULL);
 COMMIT;
 
 -- ----------------------------
@@ -135,14 +229,15 @@ CREATE TABLE `sys_role` (
   `update_time` datetime DEFAULT NULL COMMENT '最好一个更新时间',
   `remark` varchar(500) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='角色信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='角色信息表';
 
 -- ----------------------------
 -- Records of sys_role
 -- ----------------------------
 BEGIN;
 INSERT INTO `sys_role` VALUES (1, '系统管理员', 'admin', 1, 0, 0, 1, '2019-11-07 16:50:51', 1, '2019-11-07 16:50:55', '备注');
-INSERT INTO `sys_role` VALUES (2, '开发人员', 'developers', 1, 0, 0, 1, '2019-11-05 17:18:26', 1, '2019-11-05 17:36:34', '开发人员测-2试');
+INSERT INTO `sys_role` VALUES (2, '开发人员', 'developers', 1, 0, 0, 1, '2019-11-05 17:18:26', 1, '2020-01-17 19:34:24', '开发人员测-2试');
+INSERT INTO `sys_role` VALUES (5, '测试', 'test', 12, 0, 0, 1, '2020-01-17 19:34:33', 1, '2020-03-15 18:08:36', NULL);
 COMMIT;
 
 -- ----------------------------
@@ -169,6 +264,7 @@ DROP TABLE IF EXISTS `sys_role_menu`;
 CREATE TABLE `sys_role_menu` (
   `role_id` int(11) NOT NULL COMMENT '角色ID',
   `menu_id` int(11) NOT NULL COMMENT '菜单ID',
+  `option_key` varchar(255) DEFAULT NULL COMMENT '当前角色可以操作的菜单key值',
   PRIMARY KEY (`role_id`,`menu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色和菜单关联表';
 
@@ -176,10 +272,8 @@ CREATE TABLE `sys_role_menu` (
 -- Records of sys_role_menu
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_role_menu` VALUES (1, 1);
-INSERT INTO `sys_role_menu` VALUES (1, 3);
-INSERT INTO `sys_role_menu` VALUES (2, 1);
-INSERT INTO `sys_role_menu` VALUES (2, 3);
+INSERT INTO `sys_role_menu` VALUES (5, 2, '');
+INSERT INTO `sys_role_menu` VALUES (5, 15, 'add');
 COMMIT;
 
 -- ----------------------------
@@ -236,14 +330,15 @@ CREATE TABLE `sys_user` (
   `version` int(11) DEFAULT '0' COMMENT '版本号',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `uniq_login` (`login_account`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='用户信息表';
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_user` VALUES (1, '超级管理员', NULL, 'admin', '$2a$10$DddJsIctDNwGHiucMB90HegUa4iX.oszYhfQe8hXQ1nCANZfMeBeW', '$2a$04$FKmd8XmZ4xBp0vlTPAZ1NO', NULL, NULL, NULL, NULL, '2019-11-07 16:36:08', '0:0:0:0:0:0:0:1', 42, 0, 0, 1, NULL, NULL, NULL, NULL, '2019-11-07 16:36:08', 36);
-INSERT INTO `sys_user` VALUES (2, '路飞', NULL, 'luffyu', '$2a$10$DddJsIctDNwGHiucMB90HegUa4iX.oszYhfQe8hXQ1nCANZfMeBeW', '$2a$04$FKmd8XmZ4xBp0vlTPAZ1NO', NULL, NULL, NULL, NULL, '2019-11-07 16:35:16', '0:0:0:0:0:0:0:1', 41, 0, 0, 0, NULL, NULL, NULL, 1, '2019-11-07 16:35:21', 41);
+INSERT INTO `sys_user` VALUES (1, '超级管理员', NULL, 'admin', '$2a$10$DddJsIctDNwGHiucMB90HegUa4iX.oszYhfQe8hXQ1nCANZfMeBeW', '$2a$04$FKmd8XmZ4xBp0vlTPAZ1NO', NULL, NULL, NULL, NULL, '2020-03-25 15:39:08', '127.0.0.1', 172, 0, 0, 1, NULL, NULL, NULL, NULL, '2020-03-25 15:39:09', 166);
+INSERT INTO `sys_user` VALUES (2, '路飞-2', 13, 'luffyu', '$2a$10$DddJsIctDNwGHiucMB90HegUa4iX.oszYhfQe8hXQ1nCANZfMeBeW', '$2a$04$FKmd8XmZ4xBp0vlTPAZ1NO', 'qw', 'qw', NULL, 1, '2019-11-07 16:35:16', '0:0:0:0:0:0:0:1', 41, 0, 0, 0, NULL, NULL, NULL, 1, '2020-02-01 14:58:32', 41);
+INSERT INTO `sys_user` VALUES (7, '测试用户', 12, 'test', '$2a$10$XHY/x7OFv5WpraIJDKuJW.cxwdnzbTUHtP9oRtRpfc.70zf9peQRm', '$2a$06$dzSKy4DQOCcPEj5whgJHNe', '0', '1212', NULL, 0, NULL, NULL, 0, -1, 0, 0, NULL, 1, '2020-02-01 13:44:14', 1, '2020-02-01 15:06:51', 0);
 COMMIT;
 
 -- ----------------------------
@@ -262,6 +357,7 @@ CREATE TABLE `sys_user_role` (
 BEGIN;
 INSERT INTO `sys_user_role` VALUES (2, 1);
 INSERT INTO `sys_user_role` VALUES (2, 2);
+INSERT INTO `sys_user_role` VALUES (7, 1);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
