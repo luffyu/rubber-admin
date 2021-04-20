1c7f56ff7e6cb453f9d8909a5129ff84b8dd62c9
luffyu <919132691@qq.com>
2021-04-20 23:17:37 +0800

    修改参数的问题

--- rubber-admin-core/src/main/java/com/rubber/admin/core/plugins/page/PageTools.java
+++ rubber-admin-core/src/main/java/com/rubber/admin/core/plugins/page/PageTools.java
@@ -1,6 +1,7 @@
 package com.rubber.admin.core.plugins.page;
 
 import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.rubber.admin.core.base.BaseEntity;
@@ -52,8 +53,8 @@ public class PageTools {
     public static <T extends BaseEntity> IPage<T> build(PageModel pagerModel, Map<String,Class<?>> clzFiles){
         if(pagerModel.getOrder() != null && pagerModel.getSort() != null && clzFiles != null){
             for (String s : pagerModel.getSort()) {
-                if(clzFiles.get(s) == null){
-                    throw new AdminRunTimeException(AdminCode.LOGIN_AUTH_ERROR,s+"是非法成员变量");
+                if(clzFiles.get(StrUtil.toCamelCase(s)) == null){
+                    throw new AdminRunTimeException(AdminCode.PARAM_ERROR,s+"是非法成员变量");
                 }
             }
         }
